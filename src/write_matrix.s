65e2d11417cd9a15a76bb5ab8ef0b266b26dc821
huzhi <hu.zhi@outlook.com>
2022-10-16 13:07:01 +0800

    part b, task 8 wrtie matrix

--- src/write_matrix.s
+++ src/write_matrix.s
@@ -25,16 +25,112 @@
 write_matrix:
 
 	# Prologue
+	# open file
+	addi sp, sp, -20
+	sw ra, 0(sp)
+	sw a0, 4(sp)
+	sw a1, 8(sp)
+	sw a2, 12(sp)
+	sw a3, 16(sp)
+	
+	li a1, 4
+	jal ra, fopen
+	li t0, -1
+	beq a0, t0, fopen_error
+	# a4 = file_descriptor
 
+	mv a4, a0
+	lw ra, 0(sp)
+	lw a0, 4(sp)
+	lw a1, 8(sp)
+	lw a2, 12(sp)
+	lw a3, 16(sp)
+	addi sp, sp, 20
 
+	# write file - number of rows and cols
+	addi sp, sp, -20
+	sw ra, 0(sp)
+	sw a0, 4(sp)
+	sw a1, 8(sp)
+	sw a2, 12(sp)
+	sw a3, 16(sp)
+	sw a4, 20(sp)
 
+	mv a0, a4
+	mv a1, sp
+	addi a1, a1, 12
+	li a2, 2
+	li a3, 4
+	jal ra, fwrite
+	li t0, 2
+	bne a0, t0, fwrite_error
+	lw ra, 0(sp)
+	lw a0, 4(sp)
+	lw a1, 8(sp)
+	lw a2, 12(sp)
+	lw a3, 16(sp)
+	lw a4, 20(sp)
+	addi sp, sp, 20
 
+	# write file - data
+	addi sp, sp, -24
+	sw ra, 0(sp)
+	sw a0, 4(sp)
+	sw a1, 8(sp)
+	sw a2, 12(sp)
+	sw a3, 16(sp)
+	sw a4, 20(sp)
+	mv a0, a4
+	mul a2, a2, a3
+	sw a2, 24(sp)
+	li a3, 4
+	jal ra, fwrite
+	lw t0, 24(sp)
+	bne a0, t0, fwrite_error
 
+	lw ra, 0(sp)
+	lw a0, 4(sp)
+	lw a1, 8(sp)
+	lw a2, 12(sp)
+	lw a3, 16(sp)
+	lw a4, 20(sp)
+	lw a5, 24(sp)
+	addi sp, sp, 24
 
+	# close file
+	addi sp, sp, -28
+	sw ra, 0(sp)
+	sw a0, 4(sp)
+	sw a1, 8(sp)
+	sw a2, 12(sp)
+	sw a3, 16(sp)
+	sw a4, 20(sp)
+	sw a5, 24(sp)
 
-
+	mv a0, a4
+	jal ra, fclose
+	li t0, -1
+	beq a0, t0, fclose_error
+	lw ra, 0(sp)
+	lw a0, 4(sp)
+	lw a1, 8(sp)
+	lw a2, 12(sp)
+	lw a3, 16(sp)
+	lw a4, 20(sp)
+	lw a5, 24(sp)
+	addi sp, sp, 28
 
 	# Epilogue
-
-
 	ret
+
+fopen_error: 
+	li a0, 27
+	j exit
+
+fclose_error: 
+	li a0, 28
+	j exit
+
+fwrite_error:
+	li a0, 30
+	j exit
